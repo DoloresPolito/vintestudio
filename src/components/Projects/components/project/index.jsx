d1bb4874edb834577a93a6a528ffc32aa3a57e4d
Dolores Polito <dolores.polito@gmail.com>
2024-05-06 16:12:16 -0300

    projects title

--- src/components/Projects/components/project/index.jsx
+++ src/components/Projects/components/project/index.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import styles from './style.module.scss';
 import Link from 'next/link';
 import styled from "styled-components"
+import style from "./style.module.scss"
 export default function Index({index, title, manageModal, link}) {
 
     return (
@@ -13,7 +14,7 @@ export default function Index({index, title, manageModal, link}) {
         <StyledLink href={link} target="_blank">
         <div onMouseEnter={(e) => {manageModal(true, index, e.clientX, e.clientY)}} onMouseLeave={(e) => {manageModal(false, index, e.clientX, e.clientY)}} className={styles.project}>
          
-            <h2>{title}</h2>
+            <h2 className={style.worktitle}>{title}</h2>
     
             <p>Development</p>
         </div>
