033885de0784dcb95b3ad2d15586e359e00f14d1
Dolores Polito <dolores.polito@gmail.com>
2024-05-06 13:35:22 -0300

    remove'

--- src/components/WorkProgress.js
+++ src/components/WorkProgress.js
@@ -1,44 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import text from "../../public/images/creativeconcept.png";
 import dialogo from "../../public/images/discuss.png";
 import book from "../../public/images/production.png";
 import computer from "../../public/images/happyclient.png";
-import { motion, useAnimation } from "framer-motion";
-// import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
 import Image from "next/image";
 
+
 const WorkProgress2 = () => {
-  const animation = useAnimation();
-//   const { ref, inView } = useInView({ threshold: 0.2 });
-  const [shouldAnimate, setShouldAnimate] = useState(true);
 
 
-
-//   useEffect(() => {
-//     if (inView) {
-//       animation.start({
-//         x: 0,
-//         transition: {
-//           duration: 1.5,
-//           bounce: 0.1,
-//         },
-//       });
-//     }
-
-//     if (!inView) {
-//       animation.start({
-//         x: "-100vw",
-//       });
-//     }
-//   }, [inView]);
+ 
 
   return (
     <>
-      <WorkProgressSection 
-    //   ref={ref}
+      <WorkProgressSection
+    
       >
-        <Content animate={shouldAnimate ? animation : {}}>
+        <Content >
           <div>
             <Icons>
               <Image src={dialogo} alt="discuss" />
@@ -53,7 +33,10 @@ const WorkProgress2 = () => {
             </Titles>
 
             <Texts>
-              <p>Once a new client contacts us, we chat with them to get to know them better and what their needs and goals are.</p>
+              <p>
+                Once a new client contacts us, we chat with them to get to know
+                them better and what their needs and goals are.
+              </p>
             </Texts>
           </div>
 
@@ -71,7 +54,11 @@ const WorkProgress2 = () => {
             </Titles>
 
             <Texts>
-              <p>Based on the chat, we analyze and send to our customers different proposals to be able to define the most appropriate graphic concept.</p>
+              <p>
+                Based on the chat, we analyze and send to our customers
+                different proposals to be able to define the most appropriate
+                graphic concept.
+              </p>
             </Texts>
           </div>
 
@@ -89,7 +76,10 @@ const WorkProgress2 = () => {
             </Titles>
 
             <Texts>
-              <p>It's time to start producing! Definitely our favorite part in which we try to interpret the established in the previous steps.</p>
+              <p>
+                Its time to start producing! Definitely our favorite part in
+                which we try to interpret the established in the previous steps.
+              </p>
             </Texts>
           </div>
 
@@ -107,7 +97,10 @@ const WorkProgress2 = () => {
             </Titles>
 
             <Texts>
-              <p>Nothing more satisfying than a happy ending. We love to know what our clients think about the results and how we can improve.</p>
+              <p>
+                Nothing more satisfying than a happy ending. We love to know
+                what our clients think about the results and how we can improve.
+              </p>
             </Texts>
           </div>
         </Content>
@@ -163,14 +156,12 @@ const Content = styled(motion.div)`
       padding: 3px;
     }
 
-      @media only screen and (max-width: 799px) {
-        :hover {
-      scale: 1;
-      /* background-color: #f9f9f9 !important; */
+    @media only screen and (max-width: 799px) {
+      :hover {
+        scale: 1;
+        /* background-color: #f9f9f9 !important; */
+      }
     }
-  }
-
-
   }
 `;
 
@@ -209,15 +200,13 @@ const Titles = styled.div`
     line-height: 22px;
     letter-spacing: 2px;
     font-weight: 600;
-    color: #5F5F67;
+    color: #5f5f67;
     text-align: center;
     width: 250px;
     @media only screen and (max-width: 1160px) {
       /* font-size: 16px; */
       line-height: 10px;
     }
-
-
   }
 `;
 
@@ -230,7 +219,6 @@ const Numbers = styled.div`
   margin-top: 20px;
   @media only screen and (max-width: 1160px) {
     margin-top: 5px;
-    
   }
 
   @media only screen and (max-width: 799px) {
@@ -243,7 +231,7 @@ const Numbers = styled.div`
     line-height: 20px;
     letter-spacing: 1px;
     font-weight: 600;
-    color: #5F5F67;
+    color: #5f5f67;
     text-align: center;
     width: 250px;
 
@@ -268,7 +256,7 @@ const Texts = styled.div`
     line-height: 20px;
     letter-spacing: 1px;
     font-weight: 500;
-    color: #5F5F67;
+    color: #5f5f67;
     text-align: center;
     width: 230px;
 
