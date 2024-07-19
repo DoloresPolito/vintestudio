9f835932818162adce3df4698a5d9b2eeb4f0e18
Dolores Polito <dolores.polito@gmail.com>
2024-07-19 14:14:10 -0300

    final

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,15 +1,23 @@
 import styles from "./style.module.scss";
-
+// import { useRef, useState, useEffect } from "react";
+// import { useScroll, useTransform } from "framer-motion";
 import MovingTitle from "../MovingTitle";
 
 export default function Index() {
+  // const container = useRef(null);
+  // const { scrollYProgress } = useScroll({
+  //   target: container,
+  //   offset: ["start end", "end end"],
+  // });
+  // const x = useTransform(scrollYProgress, [0, 1], [0, 100]);
+  // const y = useTransform(scrollYProgress, [0, 1], [-500, 0]);
 
   return (
     <>
       <div className={styles.section}>
         <MovingTitle title="CONTACT" />
 
-        <h2>LET'S WORK TOGHETER</h2>
+        <h2>LET&apos;S WORK TOGHETER</h2>
 
         <div className={styles.contentcontainer}>
           <div className={styles.left}>
