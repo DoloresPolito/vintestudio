9c1629b964ea75a82129f528b93dfc37da0ea19d
Dolores Polito <dolores.polito@gmail.com>
2024-08-19 13:58:55 -0300

    fav?

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Roboto, Michroma } from "@next/font/google";
 import { ThemeProvider } from "../context/Themecontext";
 import "./globals.scss";
-import '../styles/fonts.css'
+import "../styles/fonts.css";
 
 const michroma = Michroma({
   subsets: ["latin"],
@@ -25,6 +25,27 @@ export default function RootLayout({ children }) {
     <html>
       <head>
         <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <link
           rel="preload"
           href="https://fonts.googleapis.com/css2?family=Michroma&display=swap"
           as="font"
@@ -40,7 +61,9 @@ export default function RootLayout({ children }) {
         />
       </head>
       <ThemeProvider>
-        <body className={`${michroma.variable} ${roboto.variable}`}>{children}</body>
+        <body className={`${michroma.variable} ${roboto.variable}`}>
+          {children}
+        </body>
       </ThemeProvider>
     </html>
   );
